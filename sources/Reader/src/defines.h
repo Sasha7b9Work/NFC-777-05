a2343e38b3ac08abcdd2890ba68c1d6d7b35c4e3
Sasha7b9Work <Sasha7b9@tut.by>
2025-11-21 22:49:52 +0300

    Удалено STM32Cube_F1

--- sources/Reader/src/defines.h
+++ sources/Reader/src/defines.h
@@ -4,9 +4,9 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_BUILD 1591
+#define VERSION_BUILD 1592
 
-#define DATE_BUILD "2025-11-21 22:48:37"
+#define DATE_BUILD "2025-11-21 22:49:51"
  
 
 #define VERSION_SCPI  "1.0"
